9f47ba485ea122dd2b814d58a61857c0f89a5c75
malishubham025 <malishubham025@gmail.com>
2023-08-07 15:34:33 +0530

    navbar complete

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 function Nav(){
+    var [position,changeposition]=React.useState(0);
+    var [opac,changeopac]=React.useState(0);
+    function handledot(event){
+        document.querySelector(".dot").style.opacity=1;
+        const e=event.target;
+        var name=event.target.name;
+        if(name=="one"){
+            changeposition(30);
+            changeopac(1);
+        }
+        if(name=="two"){
+            changeposition(125);
+            changeopac(1);
+        }
+        if(name=="three"){
+            changeposition(240);
+            changeopac(1);
+        }
+
+
+    }
+    function opacChange(){
+        changeposition(0);
+        changeopac(0);
+    }
     return(
         <div className="main-nav">
             <div className="logo">
@@ -11,11 +36,11 @@ function Nav(){
                 </div>
             </div>
             <div className="options">
-                <a>Home</a>               
-                <a>Contact</a>
-                <a>About us</a>
+                <a onMouseOver={handledot} onMouseOut={opacChange} name="one">Home</a>               
+                <a onMouseOver={handledot} onMouseOut={opacChange} name="two">Contact</a>
+                <a onMouseOver={handledot} onMouseOut={opacChange} name="three">About us</a>
                 <div className="line">
-                     <div className="dot">
+                     <div className="dot" style={{left:position,opacity:opac}}>
 
                      </div>
                 </div>
